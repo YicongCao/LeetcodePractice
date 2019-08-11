b14bb81e7ee7898e693382f8a46ee6b0f8186b16
yicongcao <yicongcao@gmail.com>
2019-08-12 00:09:27 +0800

    预先排序，来省去不必要的z序遍历（有所改善但仍然超时）

--- 15.3-sum.js
+++ 15.3-sum.js
@@ -47,14 +47,20 @@ var threeSum = function (nums) {
                 return
             }
         }
-        // triplets.push(threes.concat())
         triplets.push(threes)
     }
+    nums.sort()
     for (var x = 0; x < nums.length; x++) {
         for (var y = 0; y < nums.length; y++) {
+            if (x == y) {
+                continue
+            }
             for (var z = 0; z < nums.length; z++) {
-                if (x == y || y == z || x == z) {
+                if (y == z || x == z) {
                     continue
+                }
+                if (nums[z] > 0) {
+                    break
                 }
                 if (nums[x] + nums[y] + nums[z] == 0) {
                     appendTriplets([nums[x], nums[y], nums[z]])
